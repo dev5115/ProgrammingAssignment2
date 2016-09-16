6a9cdf41b70b4cd1bc86c61fbb080970a3f75f6e
deva5115 <devika.sunil33@gmail.com>
2016-09-16 16:46:51 +0530

    submitting the assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL                       #Initializing the value of m and x
+  set<-function(y){             #Set the value of x as y in the parent environment
+    x<<- y
+    m<<- NULL                   #When another matrix value is given to the x and m value should be reset
+  }
+  get<-function() x             #Function for Getting the value of x from the parent environment
+  setinver<-function(invers1) m <<- invers1     # m is defined in the parent environment,the value of m is assigned with the input value in this function
+  getinver<-function(invers1) m#Function for getting the value of m from parent environment
+  list(set =set,get=get,setinver=setinver,getinver=getinver)  #Each of the function is given a name and defined within a list.It is returned to the parent environment.
 }
 
 
@@ -12,4 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getinver()  #m is assigned with the inverse value of matix from parent environment in x by calling the function
+  if (!is.null(m)) { #To check whether cached value for the above matrix x is NULL or not
+      message("getting cached inverse")  #If the cached value is present then it will print above message and get the value from the parent environment
+      return(m)
+  }
+  data<-x$get() #To get the value of x from the parent environment
+  m<-solve(data,...) #solve function is used to get the inverse value of new data and store value in m variable 
+  x$setinver(m)  #To set the value of matrix in parent environment by calling the function
+  m #Implicit printing of inverse of the matrix x
 }
+
+
